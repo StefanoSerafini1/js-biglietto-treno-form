bc4396c4741a0f12e0a14160b928487dcfcc27ea
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-21 15:22:48 +0200

    variabili utili js

---
+++ myscript.js
@@ -0,0 +1,18 @@
+//variabile che ottengono input da bottoni crea e annulla
+
+var creab = document.getElementById('creaBiglietto');
+var annulla = document.getElementById('annullaBiglietto');
+
+//funzione che si attiva al click del bottone crea
+crea.addEventListener("click", function () {
+  var costob;
+  var prezzoalkm = 0.21;
+  var nome = document.getElementById('nome').value;
+  var km = document.getElementById('km').value;
+  var fasciaeta = document.getElementById('eta').value;
+  var minCarrozza = 1;
+  var maxCarrozza = 10;
+  var minnumtreno = 90000;
+  var maxnumtreno =100000;
+
+}
