1d6912dbc4a1a185b87d3a89664a65724a9bf198
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-21 16:59:23 +0200

    funzionamento con css

--- myscript.js
+++ myscript.js
@@ -4,7 +4,7 @@ var creab = document.getElementById('creaBiglietto');
 var annulla = document.getElementById('annullaBiglietto');
 
 //funzione che si attiva al click del bottone crea
-crea.addEventListener("click", function () {
+creab.addEventListener("click", function () {
   var costob;
   var prezzoalkm = 0.21;
   var nome = document.getElementById('nome').value;
@@ -20,23 +20,26 @@ crea.addEventListener("click", function () {
 
   if (fasciaeta == "Minorenne") {
         var offerta='Sconto minorenne';
-        var costob = (km * prezzoalkm - 20/100 * (km * prezzoalKm)).toFixed(2) + " euro" + "  sei minorenne è applicato uno sconto del 20%"  ;
+        var costob = (km * prezzoalkm - 20/100 * (km * prezzoalKm)).toFixed(2) + " €";
     }else if (fasciaeta == "Over 65") {
         var offerta='sconto over 65';
-        var costob = (km * prezzoalkm - 40/100 * (km * prezzoalKm)).toFixed(2) + " euro" + "  è stato applicato uno sconto del 40% per gli over 65";
+        var costob = (km * prezzoalkm - 40/100 * (km * prezzoalKm)).toFixed(2) + " €";
     }else {
         var offerta='Prezzo intero';
-        var costob = (km * prezzoalkm).toFixed(2) + " euro" + "  tariffa base prezzo intero";
+        var costob = (km * prezzoalkm).toFixed(2) + " €";
     }
     //calcolo num cp e carrozza random usando funz math random e usando valori minimi  massimali
 
     var codicecp = Math.floor(Math.random()*(maxnumcp - minnumcp + 1)) + minnumcp;
     var carrozza = Math.floor(Math.random()*(maxcarrozza - mincarrozza + 1)) + mincarrozza;
 
+    //stampa su html dei valori della tab
     document.getElementById('passeggero').innerHTML = nome;
     document.getElementById('offerta').innerHTML = offerta;
     document.getElementById('carrozza').innerHTML = carrozza;
     document.getElementById('codicecp').innerHTML = codicecp;
     document.getElementById('costob').innerHTML = costob;
+    //classe per css
+    document.getElementById('biglietto').className = "show";
 
-}
+});
