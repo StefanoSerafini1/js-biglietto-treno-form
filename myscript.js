13a8b7418b3d2ea11f55bc3021aa16903e8e3f48
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-21 15:28:47 +0200

    ciclo if per calcolo biglietto e sconto

--- myscript.js
+++ myscript.js
@@ -15,4 +15,15 @@ crea.addEventListener("click", function () {
   var minnumtreno = 90000;
   var maxnumtreno =100000;
 
+  //controllo fascia d'età tramite ciclo if
+  //con calcolo costo boglietto e applicazione sconto se necessario
+
+  if (fasciaeta == "Minorenne") {
+        var costob = (km * prezzoalkm - 20/100 * (km * prezzoalKm)).toFixed(2) + " euro" + "  sei minorenne è applicato uno sconto del 20%"  ;
+    }else if (fasciaeta == "Over 65") {
+        var costob = (km * prezzoalkm - 40/100 * (km * prezzoalKm)).toFixed(2) + " euro" + "  è stato applicato uno sconto del 40% per gli over 65";
+    }else {
+        var costob = (km * prezzoalkm).toFixed(2) + " euro" + "  tariffa base prezzo intero";
+    }
+
 }
